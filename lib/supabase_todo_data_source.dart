cbf1df30e7583cdd7ec42294721066ce8dcaee8e
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-15 14:03:24 +0900

    [add] SupabaseTodoDataSourceクラスを追加

--- lib/supabase_todo_data_source.dart
+++ lib/supabase_todo_data_source.dart
@@ -1,22 +1,22 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class TodoDataSource {
+class SupabaseTodoDataSource {
   final SupabaseClient client = Supabase.instance.client;
-  Stream<List> getTodoList() {
+  Stream<List> getAll() {
     return client.from('todos').stream(primaryKey: ['id']);
   }
 
-  Future<void> addTodoItem(String todo) async {
+  Future<void> insert(String todo) async {
     await client.from('todos').insert([
       {'todo': todo}
     ]);
   }
 
-  Future<void> deleteTodoItem(int id) async {
+  Future<void> delete(int id) async {
     await client.from('todos').delete().eq('id', id);
   }
 
-  Future<void> updateTodoItem(int id, String todo) async {
+  Future<void> update(int id, String todo) async {
     await client.from('todos').update({'todo': todo}).eq('id', id);
   }
 }
